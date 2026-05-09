cd4e90924b5d2e7509b6edbab138ad18d64a252d
JamesYuuu <yujunda20@mails.ucas.ac.cn>
2026-05-09 15:26:50 +0800

    chore(homebrew): host asset from tap release

--- Casks/photodeck.rb
+++ Casks/photodeck.rb
@@ -2,7 +2,7 @@ cask "photodeck" do
   version "1.0.2"
   sha256 "e9bb322001976ebf2ab07cc55bcb98e599f211335fac8892704d9027d8afe001"
 
-  url "https://github.com/JamesYuuu/PhotoDeck/releases/download/v#{version}/PhotoDeck.dmg"
+  url "https://github.com/JamesYuuu/homebrew-tap/releases/download/v#{version}/PhotoDeck.dmg"
   name "PhotoDeck"
   desc "Native photo viewer"
   homepage "https://github.com/JamesYuuu/PhotoDeck"
